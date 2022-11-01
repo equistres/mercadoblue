2f799eaab98f78cac1e7a2d1395051515c2d5587
Alejandro Soto <asoto@serv.externos.prismamp.com>
2022-10-31 22:15:54 -0300

    Âprimer commit

---
+++ js/inject.js
@@ -0,0 +1,40 @@
+    function processData(data){
+        let cotizacion = parseInt(data.blue.value_buy);
+
+        document.querySelectorAll("#mercadoblue").forEach(e => e.remove());
+      
+        const amounts = document.querySelectorAll("span.price-tag-amount");
+
+        amounts.forEach(am => {
+            if(am.childNodes[0].innerHTML !== '$'){
+                let price = parseFloat(am.childNodes[1].innerHTML.replace(".", ""))
+
+                var value = cotizacion*price;
+                var result = value.toLocaleString('es-ar', {
+                    style: 'currency',
+                    currency: 'ARS'
+                }).replace(",00","");
+
+                am.insertAdjacentHTML('beforeend', `<br id="mercadoblue"/><span id="mercadoblue" style="color:blue; font-size:20px!important">MB: ${result} </span>`);
+            }
+                
+        });
+
+    }
+    
+    chrome.action.onClicked.addListener((tab) => {
+
+        fetch('https://api.bluelytics.com.ar/v2/latest')
+        .then((response) => response.json())
+        .then((data) => {
+            if(tab.url.includes("mercadolibre.com.ar")) {
+                chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                function: processData,
+                args: [data]
+                });
+            }
+        });
+    
+
+    });
