4392691a785b6519dc7695a82f112a46ddba2658
Alejandro E. Soto <equistres@gmail.com>
2024-07-05 20:31:08 -0300

    Update inject.js
    agrego nuevo proveedor de cotizacion y cambios cambios de estilos de ML

--- js/inject.js
+++ js/inject.js
@@ -1,13 +1,22 @@
     function processData(data){
-        let cotizacion = parseInt(data.blue.value_buy);
+      
+        let cotizacion = parseInt(data.blue.value_sell);
 
         document.querySelectorAll("#mercadoblue").forEach(e => e.remove());
       
-        const amounts = document.querySelectorAll("span.price-tag-amount");
+        const amounts = document.querySelectorAll("span.andes-money-amount");
 
         amounts.forEach(am => {
-            if(am.childNodes[0].innerHTML !== '$'){
-                let price = parseFloat(am.childNodes[1].innerHTML.replace(".", ""))
+            
+            const nodecount = am.childNodes.length;
+
+            debugger;
+
+            var currencySymbol = nodecount> 2 ? 1 : 0;
+            var priceTag =  nodecount> 2 ? 2 : 1;
+
+            if(am.childNodes[currencySymbol].innerHTML !== '$'){
+                let price = parseFloat(am.childNodes[priceTag].innerHTML.replaceAll(".",""));
 
                 var value = cotizacion*price;
                 var result = value.toLocaleString('es-ar', {
@@ -15,9 +24,9 @@
                     currency: 'ARS'
                 }).replace(",00","");
 
-                am.insertAdjacentHTML('beforeend', `<br id="mercadoblue"/><span id="mercadoblue" style="color:blue; font-size:20px!important">MB: ${result} </span>`);
+                am.parentElement.parentElement.parentElement.insertAdjacentHTML('beforeend', `<span id="mercadoblue" style="color:blue; font-size:20px!important">MB: ${result} </span>`);
             } else {
-                let price = Number(am.childNodes[1].innerHTML.replaceAll(".",""));
+                let price = Number(am.childNodes[priceTag].innerHTML.replaceAll(".",""));
 
                 var value = price/cotizacion;
 
@@ -27,13 +36,13 @@
                 }).replace(".00", "").replace("$", "U$D ");
 
                 
-                am.insertAdjacentHTML('beforeend', `<br id="mercadoblue"/><span id="mercadoblue" style="color:green; font-size:20px!important">MB: ${result} </span>`);
+                am.parentElement.parentElement.parentElement.insertAdjacentHTML('beforeend', `<span id="mercadoblue" style="color:green; font-size:20px!important">MB: ${result} </span>`);
             }
                 
         });
-
     }
-    
+
+
     chrome.action.onClicked.addListener((tab) => {
 
         fetch('https://api.bluelytics.com.ar/v2/latest')
@@ -41,12 +50,10 @@
         .then((data) => {
             if(tab.url.includes("mercadolibre.com.ar")) {
                 chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: processData,
-                args: [data]
+                    target : {tabId : tab.id},
+                    func : processData,
+                    args : [ data ],
                 });
             }
         });
-    
-
     });
